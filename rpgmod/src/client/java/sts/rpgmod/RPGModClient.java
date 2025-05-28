09a8e3fe0af72d98b815ad235fe7f0349ab04515
ietzel <67884277+ietzel@users.noreply.github.com>
2025-05-28 18:10:00 -0400

    Add files via upload

---
+++ rpgmod/src/client/java/sts/rpgmod/RPGModClient.java
@@ -0,0 +1,10 @@
+package sts.rpgmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RPGModClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
